0db2b6578d27e231fa3945a40f68d4596d2f5520
MugeSo <mugeso@mugeso.com>
2015-02-11 19:02:27 +0900

    Move out functions from a event listener block

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@
 **/
 
 chrome.browserAction.onClicked.addListener(function () {
-
     getCurrentTab()
         .then(executeContentScript)
         .then(captureVisibleArea)
@@ -14,122 +13,122 @@ chrome.browserAction.onClicked.addListener(function () {
         .catch(function (e) {
             console.log(e);
         });
-
-    function getCurrentTab() {
-        return new Promise(function (resolve, reject) {
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                if (typeof tabs[0] === 'undefined') {
-                    reject();
-                }
-                resolve(tabs[0]);
-            });
-        });
-    }
-
-    function executeContentScript(tab) {
-        return new Promise(function (resolve) {
-            chrome.tabs.executeScript(tab.id, {file: 'content.js'}, function () {resolve(tab)});
-        });
-    }
-    function measureScreen(targetTab) {
-        return new Promise(function (resolve, reject) {
-            chrome.tabs.sendMessage(targetTab.id, {name: 'measureScreen'},
-                function (screen) { if (screen) resolve(screen); reject(new Error('Cannot measure screen.'))});
-        });
-    }
-
-    function captureVisibleArea(tab) {
-        var canvasContext;
-
-        return measureScreen(tab)
-            .then(function (screen) {
-                canvasContext = createCanvasContext(screen.width, screen.height);
-            })
-            .then(captureVisibleTab)
-            .then(function (dataUri) {
-                var img = new Image();
-                var promise = new Promise(function (resolve) {
-                    img.onload = function () {
-                        canvasContext.drawImage(img, 0, 0);
-
-                        URL.revokeObjectURL(img.src);
-                        resolve();
-                    }
-                });
-
-                img.src = dataUri;
-
-                return promise;
-            })
-            .then(function () {
-                return Promise.resolve(canvasContext.canvas.toDataURL('image/png'));
-            });
-    }
-
-    function captureVisibleTab() {
-        return new Promise(function (resolve) {
-            chrome.tabs.captureVisibleTab(null, {format: 'png'}, function (dataUri) {resolve(dataUri)});
-        });
-    }
-
-    function uploadToYabumi(dataUrl) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('post', 'https://yabumi.cc/api/images.json', true);
-        xhr.addEventListener('load', function () {
-            if (xhr.status !== 201) {
-                // TODO: send message to content script to show alert.
-                return;
-            }
-
-            var response = JSON.parse(xhr.responseText);
-            chrome.tabs.create({
-                url: response.editUrl
-            });
-            saveToClipboard(response.url);
-        });
-
-        var formData = new window.FormData();
-        formData.append('imagedata', dataURItoBlob(dataUrl), 'screen shot');
-
-        xhr.send(formData);
-
-        function dataURItoBlob(dataURI) {
-            // convert base64/URLEncoded data component to raw binary data held in a string
-            var byteString;
-            if (dataURI.split(',')[0].indexOf('base64') >= 0)
-                byteString = atob(dataURI.split(',')[1]);
-            else
-                byteString = unescape(dataURI.split(',')[1]);
-
-            // separate out the mime component
-            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-            // write the bytes of the string to a typed array
-            var ia = new Uint8Array(byteString.length);
-            for (var i = 0; i < byteString.length; i++) {
-                ia[i] = byteString.charCodeAt(i);
-            }
-
-            return new Blob([ia], {type:mimeString});
-        }
-    }
-
-    function createCanvasContext(width, height) {
-        var c = document.createElement('canvas');
-        c.width = width;
-        c.height = height;
-
-        return c.getContext('2d');
-    }
-
-    function saveToClipboard(string) {
-        var textArea = document.createElement('textarea');
-        document.body.appendChild(textArea);
-
-        textArea.value = string;
-        textArea.select();
-        document.execCommand("copy");
-
-        document.body.removeChild(textArea);
-    }
 });
+
+function getCurrentTab() {
+    return new Promise(function (resolve, reject) {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            if (typeof tabs[0] === 'undefined') {
+                reject();
+            }
+            resolve(tabs[0]);
+        });
+    });
+}
+
+function executeContentScript(tab) {
+    return new Promise(function (resolve) {
+        chrome.tabs.executeScript(tab.id, {file: 'content.js'}, function () {resolve(tab)});
+    });
+}
+function measureScreen(targetTab) {
+    return new Promise(function (resolve, reject) {
+        chrome.tabs.sendMessage(targetTab.id, {name: 'measureScreen'},
+            function (screen) { if (screen) resolve(screen); reject(new Error('Cannot measure screen.'))});
+    });
+}
+
+function captureVisibleArea(tab) {
+    var canvasContext;
+
+    return measureScreen(tab)
+        .then(function (screen) {
+            canvasContext = createCanvasContext(screen.width, screen.height);
+        })
+        .then(captureVisibleTab)
+        .then(function (dataUri) {
+            var img = new Image();
+            var promise = new Promise(function (resolve) {
+                img.onload = function () {
+                    canvasContext.drawImage(img, 0, 0);
+
+                    URL.revokeObjectURL(img.src);
+                    resolve();
+                }
+            });
+
+            img.src = dataUri;
+
+            return promise;
+        })
+        .then(function () {
+            return Promise.resolve(canvasContext.canvas.toDataURL('image/png'));
+        });
+}
+
+function captureVisibleTab() {
+    return new Promise(function (resolve) {
+        chrome.tabs.captureVisibleTab(null, {format: 'png'}, function (dataUri) {resolve(dataUri)});
+    });
+}
+
+function uploadToYabumi(dataUrl) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', 'https://yabumi.cc/api/images.json', true);
+    xhr.addEventListener('load', function () {
+        if (xhr.status !== 201) {
+            // TODO: send message to content script to show alert.
+            return;
+        }
+
+        var response = JSON.parse(xhr.responseText);
+        chrome.tabs.create({
+            url: response.editUrl
+        });
+        saveToClipboard(response.url);
+    });
+
+    var formData = new window.FormData();
+    formData.append('imagedata', dataURItoBlob(dataUrl), 'screen shot');
+
+    xhr.send(formData);
+
+    function dataURItoBlob(dataURI) {
+        // convert base64/URLEncoded data component to raw binary data held in a string
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        return new Blob([ia], {type:mimeString});
+    }
+}
+
+function createCanvasContext(width, height) {
+    var c = document.createElement('canvas');
+    c.width = width;
+    c.height = height;
+
+    return c.getContext('2d');
+}
+
+function saveToClipboard(string) {
+    var textArea = document.createElement('textarea');
+    document.body.appendChild(textArea);
+
+    textArea.value = string;
+    textArea.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(textArea);
+}
